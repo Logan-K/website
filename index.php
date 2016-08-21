d63a17b0e29de5ed29128dcf112fa8e66d5bfd46
Logan-K <lklingler0@gmail.com>
2016-08-21 14:32:52 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Logan_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<h1><?php echo the_title(); ?></h1>
+				<?php echo the_content(); ?>
+			<?php endwhile; endif; ?>
+		</main>
+	</div>
+<?php get_footer(); ?>
